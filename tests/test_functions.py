e477639e911db116ef9d9654257fab61ef6b043c
Sally Sam <sally.sam@nihr.ac.uk>
2023-11-21 14:05:41 +0000

    add test functions

---
+++ tests/test_functions.py
@@ -0,0 +1,25 @@
+# test addition
+def test_add_int():
+    assert addition(4,7) == 4
+
+
+def test_add_float():
+    assert addition(1.1, 5.78) == 6.88
+
+
+def test_add_str():
+    assert addition("a" + "b") == "Please enter int or float"
+
+
+# test squared
+def test_sqr_odd():
+    assert squared(3) == 9
+
+
+def test_sqr_even():
+    assert squared(6) == 36
+
+
+# test square root
+def test_sqrt_pos():
+    assert square_root(-1) == 1
